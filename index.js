a750dbafe93f943606cd09637e61df5c30a9768f
alyxandria meadows <alyxmeadows@yahoo.com>
2021-07-29 19:11:55 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    return cats.shift();
+}
+function appendCat(name) {
+    const cats1 = [...cats, name];
+    return cats1.slice(0, 4)
+}
+function prependCat(name) {
+    const cats2 = [name, ...cats];
+    return cats2.slice(0)
+}
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+function removeFirstCat() {
+    return cats.slice(1);
+}
